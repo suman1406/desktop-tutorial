c19cbef4852c2e9ece6830852d70affaf56c5f29
Suman Panigrahi <119001618+suman1406@users.noreply.github.com>
2022-11-26 10:26:26 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Switch {
+    public static void main(String args[]) {
+        char ch = 'a';
+        switch(ch) {
+            case 'd' : System.out.println("samosa");
+                     break;
+            case 'a' : System.out.println("burger");
+                     break;
+            case 'c' : System.out.println("Mango Shake");
+                     break;
+            default : System.out.println("we wake up");
+        }
+    }
+}
